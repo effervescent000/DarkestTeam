7ab6e4257c3b261266b63b26b764d42293a0f1ed
effervescent000 <kislio01@gmail.com>
2018-12-06 10:07:53 -0500

    more changes to importing/exporting roster

--- RosterWindowController.java
+++ RosterWindowController.java
@@ -17,6 +17,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -46,7 +47,7 @@ public class RosterWindowController implements Initializable {
     private IOSave ioSave = new IOSave();
 
     @FXML
-    private Button addToRosterButton, removeFromRosterButton, finalizeButton, 
+    private Button addToRosterButton, removeFromRosterButton, finalizeButton,
             removeButton, editButton, exportButton, importButton, upButton, downButton;
 
     /**
@@ -61,19 +62,19 @@ public class RosterWindowController implements Initializable {
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("heroName"));
         classColumn.setCellValueFactory(new PropertyValueFactory<>("heroClass"));
         lvlColumn.setCellValueFactory(new PropertyValueFactory<>("resolveLvl"));
-        rosterDump.setItems(roster.getHeroArray());
+        rosterDump.setItems(roster.getHeroBench());
 
         nameSelectedCol.setCellValueFactory(new PropertyValueFactory<>("heroName"));
         classSelectedCol.setCellValueFactory(new PropertyValueFactory<>("heroClass"));
         lvlSelectedCol.setCellValueFactory(new PropertyValueFactory<>("resolveLvl"));
 
-        if (roster.getHeroRoster().isEmpty()) {
+        if (roster.getSelectedHeroes().isEmpty()) {
             if (selRosterList == null) {
                 selRosterList = FXCollections.observableArrayList();
             }
             selRosterTable.setItems(selRosterList);
         } else {
-            selRosterTable.setItems(roster.getHeroRoster());
+            selRosterTable.setItems(roster.getSelectedHeroes());
         }
 
     }
@@ -84,7 +85,7 @@ public class RosterWindowController implements Initializable {
         roster.addHero("Mary", "Vestal", 2);
         roster.addHero("Miri", "Arbalest", 1);
 
-        roster.setHeroRoster(roster.getHeroArray());
+        roster.setSelectedHeroes(roster.getHeroBench());
 
     }
 
@@ -105,7 +106,7 @@ public class RosterWindowController implements Initializable {
             Hero selection = selRosterTable.getSelectionModel().getSelectedItem();
             selRosterList.remove(selection);
         } else if (event.getSource().equals(finalizeButton)) {
-            roster.setHeroRoster(selRosterList);
+            roster.setSelectedHeroes(selRosterList);
 
             Stage stage = (Stage) finalizeButton.getScene().getWindow();
             stage.close();
@@ -129,11 +130,36 @@ public class RosterWindowController implements Initializable {
                 Logger.getLogger(RosterWindowController.class.getName()).log(Level.SEVERE, null, ex);
             }
         } else if (event.getSource().equals(exportButton)) {
-            ioSave.buildXML();
-            ioSave.dumpHeroes();
+//            ioSave.dumpHeroes();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Export status");
+            alert.setHeaderText(null);
+            if (roster.getHeroBench().isEmpty()) {
+                alert.setContentText("Nothing to export.");
+                alert.showAndWait();
+            } else if (ioSave.buildXML(roster)) {
+                alert.setContentText("Roster exported successfully!");
+                alert.showAndWait();
+            } else {
+                alert.setContentText("Roster export failed.");
+                alert.showAndWait();
+            }
+//            ioSave.buildXML(roster);
         } else if (event.getSource().equals(importButton)) {
-//            ioSave.readXML();
-            ioSave.parseSave();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Import status");
+            alert.setHeaderText(null);
+            roster = ioSave.readXML();
+            if (roster == null) {
+                alert.setContentText("Roster import failed.");
+                alert.showAndWait();
+            } else {
+                alert.setContentText("Roster imported successfully!");
+                alert.showAndWait();
+            }
+
+//            rosterDump.setItems(roster.getHeroBench());
+//            ioSave.parseSave();
         }
     }
 
